a5557321597bdc789c2005ee9c56bb65f2dd355d
cmchang1209 <cmchang120956@gmail.com>
2022-05-27 04:29:10 +0000

    equipment add & update and add view pv

--- resources/js/views/components/view/Pv.vue
+++ resources/js/views/components/view/Pv.vue
@@ -19,11 +19,12 @@ export default {
         src() {
             var url =''
             var p_80 = this.ports.find(item => {
-                return item.port_name = 80
+                return item.port_name === 80
             })
             var p_8000 = this.ports.find(item => {
-                return item.port_name = 8000
+                return item.port_name === 8000
             })
+            console.log(p_80)
             if(p_80 && p_8000) {
                 url = `http:fidolive.ga:${p_80.port_no}/pc?p_8000=${p_8000.port_no}`
             }
