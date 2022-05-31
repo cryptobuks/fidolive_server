12ebf0513681a176fa1e6411fa4661e8ad83ccf5
cmchang1209 <cmchang120956@gmail.com>
2022-05-31 08:56:20 +0000

    equipment add & update and add view pv

--- resources/js/views/components/equipment/List.vue
+++ resources/js/views/components/equipment/List.vue
@@ -65,7 +65,9 @@ export default {
         },
         changeConnectEquipment() {
             console.log('changeConnectEquipment')
-            this.fetchData()
+            setTimeout(() => {
+                this.fetchData()
+            },3000)
         },
         disconnect() {
             console.log('disconnect')
